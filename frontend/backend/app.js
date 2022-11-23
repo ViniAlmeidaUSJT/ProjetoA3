8479718db61c9519146b867ef1d15b0b48558a6b
Nathan <nathanaraujo025@gmail.com>
2022-11-22 22:38:06 -0300

    aula 22-11 / refazendo aplicação do professor anderson

---
+++ frontend/backend/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const clienteRoutes = require('./rotas/rotas');
+//const Cliente = require('./models/cliente');
+
+//IMPORTANTE: Troque pela sua conexão
+mongoose.connect('mongodb+srv://vinialmeida05:viniciussantos05@serverprojeto.fupc2id.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Conexão Ok');
+  }).catch(() => {
+    console.log('Sem conexão');
+  });
+
+const clientes = [
+  {
+    id: '1',
+    nome: 'Jose',
+    fone: '11223344',
+    email: 'jose@email.com'
+  },
+  {
+    id: '2',
+    nome: 'Jaqueline',
+    fone: '22112211',
+    email: 'jaqueline@email.com'
+  }
+];
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
+});
+
+app.use('/api/clientes/', clienteRoutes);
+
+module.exports = app;
